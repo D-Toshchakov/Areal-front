c8d20b807367d5655bde61a611fcc2bbd10267a5
D-Toshchakov <daniiltoshchakov@gmail.com>
2023-03-13 19:10:36 +0300

    installed vuetify and axios. Started creating components for articles

---
+++ src/components/Article.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>{{ article.articleName }}</h3>   
+        {{ article.articleText }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        article: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
---
+++ src/components/ArticlesPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <Article v-for="article in articles" v-bind:article="article"/>
+  </v-container>
+</template>
+
+<script>
+import Article from './Article.vue';
+import axios from 'axios'
+
+export default {
+  components: {
+    Article
+  },
+  
+  data: () => ({
+    articles: []
+  }),
+  
+  mounted() {
+    this.fetchAllArticles()
+  },
+
+  methods: {
+    fetchAllArticles() {
+      axios.get('http://localhost:3000/article')
+        .then(response => this.articles = response.data)
+    }
+  }
+}
+</script>
